8a462625b5fdfd088a95df584570666e11634ac3
ElinaAsk06 <elinanurlan06@gmail.com>
2022-08-10 19:41:06 +0600

    fdf

---
+++ greeting.js
@@ -0,0 +1,14 @@
+const name = prompt("ваше имя?")
+ alert ('Hello, ' + name + "!")
+
+const arr = ["Polina", true, false, 16]
+console.log(arr)
+
+const arr1 = ["Amina", 10]
+console.log(arr1)
+
+ if(arr.length !== arr1.length){
+   console.log(false)
+ } else if(arr.length === arr1.length){
+     console.log(true)
+ }
